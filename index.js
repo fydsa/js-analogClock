41aaa9ee0c54776031741659c722e05aa2951140
Saba <saba.alikhani@ymail.com>
2021-09-08 14:58:04 +0430

    Fixed the bugs on locating the hands

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const hand_minute = document.querySelector(".hand-minute");
 const hand_second = document.querySelector(".hand-second");
 
 function timeHandler() {
-  const time = new Date();
+  let time = new Date();
 
-  const hour = time.getHours();
-  const minute = time.getMinutes();
-  const second = time.getSeconds();
-
-  console.log("the min??", minute, hour / 12 + minute / 60);
+  let hour = time.getHours();
+  let minute = time.getMinutes();
+  let second = time.getSeconds();
 
   hand_hour.style.transform = `rotate(${
     (hour / 12 + minute / (60 * 12)) * 360
   }deg)`;
-  hand_minute.style.transform = `rotate(${(minute / 60) * 360}deg)`;
+
+  hand_minute.style.transform = `rotate(${
+    (minute / 60 + second / (60 * 60)) * 360
+  }deg)`;
+
   hand_second.style.transform = `rotate(${(second / 60) * 360}deg)`;
 }
 
